d13bee6037a5c29f9e1aaac35c7fda86e2de45bc
JAVIER <JAVIER@JAVIER-PC>
2017-02-07 19:17:54 -0500

    :(

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author JAVIER
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int ordenarNumeros(int a,int b, int c){
+            int d = 0;
+            if (a > b){
+                if ( a > c){
+             d = a;
+            } 
+                else if( c > a) {
+                    if (c > b){
+                        d = c;
+                    }      
+                
+            }
+            
+        } else {
+                if (b > c){
+                    d = b;
+                }
+            }
+            return d;
+    } 
+    public static void main(String[] args) {
+      
+        System.out.println("hola mundo");
+        
+        }
+
+    
